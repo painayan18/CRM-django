cfec7db2d5aff8454e101a8b6a62c92a58ef60e6
phant <nayanspai@gmail.com>
2024-12-03 15:06:39 -0600

    restructured

--- crm/urls.py
+++ crm/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CRM project.
+URL configuration for crm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
