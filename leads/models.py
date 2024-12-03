fb1cc9c4b93a8c27a0126b14eafd8b968438bd7a
phant <nayanspai@gmail.com>
2024-12-03 15:21:24 -0600

    restructure & refactor

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Lead(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+
